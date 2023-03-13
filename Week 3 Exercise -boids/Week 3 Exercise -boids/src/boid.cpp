7e3589b5aa771e05555fa7d7a693256670babcdb
lingyizhang-ual <119836213+lingyizhang-ual@users.noreply.github.com>
2023-03-13 15:05:20 +0000

    Add files via upload

---
+++ Week 3 Exercise -boids/Week 3 Exercise -boids/src/boid.cpp
@@ -0,0 +1,201 @@
+/*
+ *  boid.cpp
+ *  boids
+ *
+ *  Created by Marco Gillies on 05/10/2010.
+ *  Copyright 2010 Goldsmiths, University of London. All rights reserved.
+ *
+ */
+
+#include "boid.h"
+#include "ofMain.h"
+
+Boid::Boid()
+{
+	separationWeight = 1.0f;
+	cohesionWeight = 0.2f;
+	alignmentWeight = 0.2f;
+	
+	separationThreshold = 15;
+	neighbourhoodSize = 100;
+	
+	position = ofVec3f(ofRandom(50, -100), ofRandom(-30, 50));
+	velocity = ofVec3f(ofRandom(1, 2), ofRandom(-10, -2));
+}
+
+Boid::Boid(ofVec3f &pos, ofVec3f &vel)
+{
+	separationWeight = 1.0f;
+	cohesionWeight = 0.2f;
+	alignmentWeight = 0.9f;
+	
+	separationThreshold = 10;
+	neighbourhoodSize = 300;
+	
+	position = pos;
+	velocity = vel;
+}
+
+Boid::~Boid()
+{
+	
+}
+
+float Boid::getSeparationWeight()
+{
+	return separationWeight;
+}
+float Boid::getCohesionWeight()
+{
+	return cohesionWeight;
+}
+
+float Boid::getAlignmentWeight()
+{
+	return alignmentWeight;
+}
+
+
+float Boid::getSeparationThreshold()
+{
+	return separationThreshold;
+}
+
+float Boid::getNeighbourhoodSize()
+{
+	return neighbourhoodSize;
+}
+
+
+void Boid::setSeparationWeight(float f)
+{
+	separationWeight = f;
+}
+void Boid::setCohesionWeight(float f)
+{
+	cohesionWeight = f;
+}
+
+void Boid::setAlignmentWeight(float f)
+{
+	alignmentWeight = f;
+}
+
+
+void Boid::setSeparationThreshold(float f)
+{
+	separationThreshold = f;
+}
+
+void Boid::setNeighbourhoodSize(float f)
+{
+	neighbourhoodSize = f;
+}
+
+
+ofVec3f Boid::getPosition()
+{
+	return position;
+}
+
+ofVec3f Boid::getVelocity()
+{
+	return velocity;
+}
+
+ofVec3f Boid::separation(std::vector<Boid *> &otherBoids)
+{
+	// finds the first collision and avoids that
+	// should probably find the nearest one
+	// can you figure out how to do that?
+	for (int i = 0; i < otherBoids.size(); i++)
+	{	
+		if(position.distance(otherBoids[i]->getPosition()) < separationThreshold)
+		{
+			ofVec3f v = position -  otherBoids[i]->getPosition();
+			v.normalize();
+			return v;
+		}
+	}
+}
+
+ofVec3f Boid::cohesion(std::vector<Boid *> &otherBoids)
+{
+	ofVec3f average(0,0,0);
+	int count = 0;
+	for (int i = 0; i < otherBoids.size(); i++)
+	{
+		if (position.distance(otherBoids[i]->getPosition()) < neighbourhoodSize)
+		{
+			average += otherBoids[i]->getPosition();
+			count += 1;
+		}
+	}
+	average /= count;
+	ofVec3f v =  average - position;
+	v.normalize();
+	return v;
+}
+
+ofVec3f Boid::alignment(std::vector<Boid *> &otherBoids)
+{
+	ofVec3f average(0,3,0);
+	int count = 0;
+	for (int i = 0; i < otherBoids.size(); i++)
+	{
+		if (position.distance(otherBoids[i]->getPosition()) < neighbourhoodSize)
+		{
+			average += otherBoids[i]->getVelocity();
+			count += 1;
+		}
+	}
+	average /= count;
+	ofVec3f v =  average - velocity;
+	v.normalize();
+	return v;
+}
+
+void Boid::update(std::vector<Boid *> &otherBoids, ofVec3f &min, ofVec3f &max)
+{
+	velocity += separationWeight*separation(otherBoids);
+	velocity += cohesionWeight*cohesion(otherBoids);
+	velocity += alignmentWeight*alignment(otherBoids);
+	
+	walls(min, max);
+	position += velocity;
+}
+
+void Boid::walls(ofVec3f &min, ofVec3f &max)
+{
+	if (position.x < min.x){
+		position.x = min.x;
+		velocity.x *= -1;
+	} else if (position.x > max.x){
+		position.x = max.x;
+		velocity.x *= -1;
+	}
+	
+	if (position.y < min.y){
+		position.y = min.y;
+		velocity.y *= -1;
+	} else if (position.y > max.y){
+		position.y = max.y;
+		velocity.y *= -1;
+	}
+	
+	
+}
+
+void Boid::draw()
+{
+	ofSetColor(9,215,1);
+    //ofDrawCircle(position.x, position.y, 5);
+    //ofDrawTriangle(position.x,position.y,10,40,90,40);
+    ofDrawRectangle(position.x,position.y,10,10);
+    //ofDrawLine(position.x,position.y,19,8,10);
+}
+void Boid::drawnew()
+{
+    ofSetColor(100,17,23);
+    ofDrawRectangle(position.x, position.y,-10,-10);
+}
